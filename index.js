c190bfec5837b84af48f50683fb6af160a3093a9
katiecsomers <kcornallysomers@gmail.com>
2022-10-22 02:30:55 +1300

    added further default city

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function updateTime() {
 		.tz("Europe/Berlin")
 		.format("Do MMM YYYY");
 
-	let istanbulTimeElement = document.querySelector(".istanbul .time");
-	istanbulTimeElement.innerHTML = moment
-		.tz("Europe/Istanbul")
+	let berlinTimeElement = document.querySelector(".berlin .time");
+	berlinTimeElement.innerHTML = moment
+		.tz("Europe/Berlin")
 		.format("[<small>]ddd[</small>] HH:mm:ss");
 
 	let istanbulDateElement = document.querySelector(".istanbul .date");
@@ -14,35 +14,44 @@ function updateTime() {
 		.tz("Europe/Istanbul")
 		.format("Do MMM YYYY");
 
-	let berlinTimeElement = document.querySelector(".berlin .time");
-	berlinTimeElement.innerHTML = moment
-		.tz("Europe/Berlin")
+	let istanbulTimeElement = document.querySelector(".istanbul .time");
+	istanbulTimeElement.innerHTML = moment
+		.tz("Europe/Istanbul")
+		.format("[<small>]ddd[</small>] HH:mm:ss");
+
+	let maritiusDateElement = document.querySelector(".mauritius .date");
+	maritiusDateElement.innerHTML = moment
+		.tz("Indian/Mauritius")
+		.format("Do MMM YYYY");
+
+	let mauritiusTimeElement = document.querySelector(".mauritius .time");
+	mauritiusTimeElement.innerHTML = moment
+		.tz("Indian/Mauritius")
 		.format("[<small>]ddd[</small>] HH:mm:ss");
 }
 
 function showTime(event) {
-	if (event.target.value.length > 0)
-		setInterval(() => {
-			let newCity = document.querySelector(".default-city");
-			let cityTimeZone = event.target.value;
+	clearInterval(refreshIntervalId);
 
-			if (cityTimeZone === "current") {
-				cityTimeZone = moment.tz.guess();
-			}
+	setInterval(() => {
+		let newCity = document.querySelector(".default-city");
+		let cityTimeZone = event.target.value;
 
-			cityName = cityTimeZone.split("/")[1];
+		if (cityTimeZone === "current") {
+			cityTimeZone = moment.tz.guess();
+		}
+		cityName = cityTimeZone.split("/")[1];
 
-			newCity.innerHTML = `<div class="city-info">
+		newCity.innerHTML = `<div class="city-info">
 				<div><h2 class="city-name">${cityName}</h2>
-					<div class="date"> ${moment.tz(event.target.value).format("Do MMM YYYY")} </div>
+					<div class="date"> ${moment.tz(cityTimeZone).format("Do MMM YYYY")} </div>
 				</div>
 				<div class="time"> ${moment
-					.tz(event.target.value)
+					.tz(cityTimeZone)
 					.format("[<small>]ddd[</small>] HH:mm:ss")} </div>
-			</div>`;
-
-			clearInterval(refreshIntervalId);
-		}, 1000);
+			</div>
+      <a href="/">Default cities homepage</a>`;
+	}, 1000);
 }
 
 let refreshIntervalId = setInterval(updateTime, 1000);
